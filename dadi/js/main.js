7a488660c18bf3317de93e5d887d58e33391f073
Samuelimmerso <samuelimmerso.info@gmail.com>
2023-01-29 00:57:40 +0100

    debug

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,8 +1,8 @@
-//possible numbers
-const numbers = [1, 2, 3, 4, 5, 6];
-let user, computer;
-let spear = document.querySelector(".spear");
-let results = document.querySelector(".results");
+//declarations
+const numbers = [1, 2, 3, 4, 5, 6]; //possible numbers
+let user, computer; // user and computer dice
+let spear = document.querySelector(".spear"); // button for spear of the dice
+let results = document.querySelector(".results"); // results output span
 
 
 //if user click the button
@@ -11,12 +11,14 @@ spear.addEventListener("click",
         user = Math.floor((Math.random() * 6) + 1); //rand number user on dice
         computer = Math.floor((Math.random() * 6) + 1); // rand number computer on dice
 
-
+        //output numbers
         results.innerHTML = `Utente: <span class="user">${user}</span> Computer: <span class="computer">${computer}</span> `; //output numbers
-        let user_el = document.querySelector(".user");
-        let computer_el = document.querySelector(".computer");
+        let user_el = document.querySelector(".user"); // class of user element
+        let computer_el = document.querySelector(".computer"); // class of computer element
 
         // verify who are the winner
+        // the greater green and the lower red
+        // yellow if are equals
         if (user > computer) {
             user_el.classList.add("greater");
             computer_el.classList.add("lower");
